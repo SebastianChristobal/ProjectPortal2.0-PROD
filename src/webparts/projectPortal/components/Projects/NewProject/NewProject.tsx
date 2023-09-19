472ec94d3955d9f30af67b89cd7330263e32ad0b
Sebastian Pavez <sebastiang.pavez@gmail.com>
2023-09-19 20:01:30 +0200

    test

--- src/webparts/projectPortal/components/Projects/NewProject/NewProject.tsx
+++ src/webparts/projectPortal/components/Projects/NewProject/NewProject.tsx
@@ -228,7 +228,7 @@ const NewProject: React.FC<INewProjectProps> = (props) =>{
             <Label
              style={{fontSize:18, fontWeight: 400}}
             >
-           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
+           Lorem ipsum dolor sit amet samet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
             </Label>
         </div>
     </div>
