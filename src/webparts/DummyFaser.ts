0b040b4027326eb50a73a416a24708f3884be032
SebastianChristobal <sebastiang.pavez@gmail.com>
2024-04-01 22:57:55 +0200

    DummFaser

---
+++ src/webparts/DummyFaser.ts
@@ -0,0 +1,29 @@
+
+
+
+export interface Faser{
+    id?:number;
+    title:string;
+    description:string;
+}
+
+
+export const progressSteps : Faser[] = [
+    {
+        title: "Fas1",
+        description: "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+        id:1
+
+    },
+    {
+        title: "Fas2",
+        description: "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+        id:2
+
+    },
+    {
+        title: "Fas3",
+        description: "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+        id:3
+    }
+]
